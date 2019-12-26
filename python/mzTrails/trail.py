685c532487029b3e0327ab7e6abc88727756dc93
Brent Forrest <forrest.brent@gmail.com>
2019-12-26 15:07:26 +0900

    rebuild with a simpler setup

--- python/mzTrails/trail.py
+++ python/mzTrails/trail.py
@@ -1,5 +1,5 @@
 import das
-
+import bakeUtils
 class Blaze(object):
 
     def __init__(self, name="trail", sections=5, p=None, emit=None, frange=[]):
@@ -15,6 +15,7 @@ class Blaze(object):
         self.name = name
         self.maxDiv = 0
         self.sections = sections
+        self.reduce = 0.7
 
         if not frange:
             frange=self.getfrange()
@@ -35,33 +36,36 @@ class Blaze(object):
         surf = maya.cmds.createNode("nurbsSurface",         n="%s_SURF" % self.name)
         loft = maya.cmds.createNode("loft",                 n="%s_OUT" % self.name)
         cfos = maya.cmds.createNode("curveFromSurfaceIso",  n="%s_cfos" % self.name)
-        addl = maya.cmds.createNode("addDoubleLinear" ,     n="%s_add" % self.name)
-        plmn = maya.cmds.createNode("plusMinusAverage",     n="%s_minus" % self.name)
-        mult = maya.cmds.createNode("multiplyDivide",       n="%s_multi" % self.name)
-        five = maya.cmds.createNode("addDoubleLinear",      n="%s_pointFive" % self.name)
+        mins = maya.cmds.createNode("addDoubleLinear",      n="%s_minus" % self.name)
+        rebu = maya.cmds.createNode("rebuildCurve",         n="%s_rebuild" % self.name)
+        choi = maya.cmds.createNode("choice",               n="%s_choice" % self.name)
+        clmp = maya.cmds.createNode("clamp",                n="%s_clamp" % self.name)
 
-        maya.cmds.addAttr(curv, ln="frame", at="float", min = self.frange[0]-2, max = self.frange[1]+2, k=True)
-        maya.cmds.addAttr(curv, ln="floor", at="long", k=True)
+        maya.cmds.addAttr(curv, ln="frame", at="float", min = self.frange[0], max = self.frange[1], k=True)
+        maya.cmds.addAttr(curv, ln="spanType", at="enum", en="frame:uniform:", k=True)
+        maya.cmds.addAttr(curv, ln="count", at="long", min=3, k=True)
 
+        maya.cmds.connectAttr("%s.outputCurve" % cfos, "%s.inputCurve" % rebu )
+        maya.cmds.connectAttr("%s.outputCurve" % rebu, "%s.create" % curv )
         maya.cmds.connectAttr("%s.outputSurface" % loft, "%s.create" % surf )
         maya.cmds.connectAttr("%s.outputSurface" % loft, "%s.inputSurface" % cfos )
-        maya.cmds.connectAttr("%s.outputCurve" % cfos, "%s.create" % curv )
-        maya.cmds.connectAttr("%s.frame" % curv, "%s.input1" % addl)
-        maya.cmds.connectAttr("%s.output" % addl, "%s.floor" % curv)
-        maya.cmds.connectAttr("%s.frame" % curv, "%s.input1D[0]" % plmn)
-        maya.cmds.connectAttr("%s.floor" % curv, "%s.input1D[1]" % plmn)
-        maya.cmds.connectAttr("%s.output1D" % plmn, "%s.input1X" % mult)
-        maya.cmds.connectAttr("%s.outputX" % mult, "%s.input1" % five)
-        maya.cmds.connectAttr("%s.output" % five, "%s.isoparmValue" % cfos)
+        maya.cmds.connectAttr("%s.spanType" % curv, "%s.selector" % choi )
+        maya.cmds.connectAttr("%s.frame" % curv, "%s.input1" % mins)
+        maya.cmds.connectAttr("%s.output" % mins, "%s.isoparmValue" % cfos)
+        maya.cmds.connectAttr("%s.output" % mins, "%s.inputR" % clmp)
+        maya.cmds.connectAttr("%s.outputR" % clmp, "%s.input[0]" % choi)
+        maya.cmds.connectAttr("%s.count" % curv, "%s.input[1]" % choi)
+        maya.cmds.connectAttr("%s.output" % choi, "%s.spans" % rebu)
 
         maya.cmds.setAttr("%s.isoparmDirection" % cfos, 1)
-        maya.cmds.setAttr("%s.relativeValue" % cfos, 1)
-        maya.cmds.setAttr("%s.input2" % addl, -0.5)
-        maya.cmds.setAttr("%s.operation" % plmn, 2)
-        maya.cmds.setAttr("%s.operation" % mult, 2)
-        maya.cmds.setAttr("%s.input2X" % mult, sections)
-        maya.cmds.setAttr("%s.input2" % five, 0.5)
+        maya.cmds.setAttr("%s.relativeValue" % cfos, 0)
+        maya.cmds.setAttr("%s.maxR" % clmp, self.frange[1]+2)
+        maya.cmds.setAttr("%s.minR" % clmp, 3)
+
         maya.cmds.setAttr("%s.dispCV" % curv, 1)
+        maya.cmds.setAttr("%s.uniform" % loft, 1)
+        maya.cmds.setAttr("%s.input2" % mins, -self.frange[0])
+        
         
         transform = maya.cmds.listRelatives(surf, p=True)[0]
         maya.cmds.parent(transform, grup)
@@ -70,11 +74,7 @@ class Blaze(object):
                     "curve" : curv,
                     "surface" : surf,
                     "loft" : loft,
-                    "curveFromSurface" : cfos,
-                    "addLinear" : addl,
-                    "minus" : plmn,
-                    "divide" : mult,
-                    "pointFive" : five}
+                    "curveFromSurface" : cfos}
 
         for node in nodes:
             self.nodes[node] = nodes[node]
@@ -84,7 +84,7 @@ class Blaze(object):
         frame = maya.cmds.currentTime(q=True)
         cid=0
         prepend = []
-        for i in range(int(self.frange[0]-2), int(self.frange[1]+2)):
+        for i in range(int(self.frange[0]), int(self.frange[1]+1)):
             maya.cmds.refresh(su=True)
             maya.cmds.currentTime(i)
             particles = maya.cmds.getAttr("%s.position" % p) or []
@@ -110,34 +110,19 @@ class Blaze(object):
             self.maxDiv = maya.cmds.getAttr("%s.spans" % c)
 
         numCurves = len(self.nodes.curves)
-
-        for i in range(sections):
-            chNode = maya.cmds.createNode("choice", n="%s_%02d" % ("choice", i))
-            raNode = maya.cmds.createNode("setRange", n="%s_%02d" % ("setRange", i))
-            self.nodes.choiceNodes.append(chNode)
-            self.nodes.rangeNodes.append(raNode)
-
-            maya.cmds.setAttr("%s.oldMinX" % raNode, self.frange[0])
-            maya.cmds.setAttr("%s.oldMaxX" % raNode, numCurves-sections)
-            maya.cmds.setAttr("%s.minX" % raNode, i)
-            maya.cmds.setAttr("%s.maxX" % raNode, numCurves+i-sections)
-
-            maya.cmds.connectAttr("%s.floor" % self.nodes.curve, "%s.valueX" % raNode)
-            maya.cmds.connectAttr("%s.outValueX" % raNode, "%s.selector" % chNode)
-            maya.cmds.connectAttr("%s.output" % chNode, "%s.inputCurve[%s]" % (self.nodes.loft, i))
             
-            for c in self.nodes.curves:
-                maya.cmds.rebuildCurve(c, s=self.maxDiv)
-                id = self.nodes.curves[c].id
-                shape = self.nodes.curves[c].shape
-                maya.cmds.connectAttr("%s.worldSpace[0]" % shape, "%s.input[%s]" % (chNode, id))
+        for c in self.nodes.curves:
+            maya.cmds.rebuildCurve(c, s=self.maxDiv)
+            id = self.nodes.curves[c].id
+            shape = self.nodes.curves[c].shape
+            maya.cmds.connectAttr("%s.worldSpace[0]" % shape, "%s.inputCurve[%s]" % (self.nodes.loft, id))
 
         maya.cmds.setAttr("%s.v" % self.nodes.group, False)
         maya.cmds.setAttr("%s.frame" % self.nodes.curve, frame)
         maya.cmds.refresh(su=False)
         
-        maya.cmds.currentTime(e=frame)
-        maya.cmds.select(self.nodoes.curve)
+        maya.cmds.currentTime(frame)
+        maya.cmds.select(self.nodes.curve)
 
         if verbose or dryrun:
             das.pprint(self.nodes)
@@ -145,3 +130,5 @@ class Blaze(object):
 trail = Blaze(p="nParticleShape1", emit="emitter1")
 trail.build()
 trail.draw()
+
+maya.cmds.getAttr("emitter1.t", time=(0))[0]
